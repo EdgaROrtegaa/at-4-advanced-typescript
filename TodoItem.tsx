8d3e6e09ed02be3ff55b4d24d33d393671ea1f29
EdgaROrtegaa <147570628+EdgaROrtegaa@users.noreply.github.com>
2024-07-02 23:19:16 -0700

    Update TodoItem.tsx

--- TodoItem.tsx
+++ TodoItem.tsx
@@ -1,18 +1,39 @@
-// src/components/TodoItem.tsx
-import React from 'react';
+// src/components/TodoList.tsx
+import React, { useState, useEffect } from 'react';
+import TodoItem from './TodoItem';
+import { Todo } from '../types';
 
-interface TodoItemProps {
-  text: string;
-  completed: boolean;
-  onToggle: () => void;
-}
+const TodoList: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
 
-const TodoItem: React.FC<TodoItemProps> = ({ text, completed, onToggle }) => {
+  useEffect(() => {
+    // Fetch todos from API
+    const fetchTodos = async () => {
+      const response = await fetch('/api/todos');
+      const data: Todo[] = await response.json();
+      setTodos(data);
+    };
+    fetchTodos();
+  }, []);
+
+  const toggleTodo = (id: number) => {
+    setTodos(todos.map(todo => 
+      todo.id === id ? { ...todo, completed: !todo.completed } : todo
+    ));
+  };
+
   return (
-    <li onClick={onToggle} style={{ textDecoration: completed ? 'line-through' : 'none' }}>
-      {text}
-    </li>
+    <ul>
+      {todos.map(todo => (
+        <TodoItem 
+          key={todo.id} 
+          text={todo.text} 
+          completed={todo.completed} 
+          onToggle={() => toggleTodo(todo.id)} 
+        />
+      ))}
+    </ul>
   );
 };
 
-export default TodoItem;
+export default TodoList;
